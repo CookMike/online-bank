88b78baef7ac85123fda50fa761e2bafc8163acd
Michal Kuchar <kucharmichalnl@gmail.com>
2021-07-16 15:50:18 +0100

    lazy loading

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
+
 const overlay = document.querySelector('.overlay');
+
 const btnCloseModal = document.querySelector('.btn--close-modal');
+
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
@@ -27,6 +30,7 @@ document.addEventListener('keydown', function (e) {
 });
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+
 const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', e => {
@@ -35,33 +39,119 @@ btnScrollTo.addEventListener('click', e => {
 
 document.querySelector('.nav__links').addEventListener('click', e => {
   e.preventDefault();
-  console.log(e.target);
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+//tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
 
-const uniqueInOrder = [1, 2, 2, 3, 3];
-const unique = [];
-if (typeof uniqueInOrder === Object) {
-  const sep = (uniqueInOrder + '').split(',');
-  console.log(sep);
+const tabsContainer = document.querySelector('.operations__tab-container');
 
-  sep.forEach((x, i, _) => {
-    if (sep[i] !== sep[i + 1]) {
-      unique.push(x);
-    }
-  });
-  console.log(unique);
-}
-// events
+const tabsContent = document.querySelectorAll('.operations__content');
 
-const h1 = document.querySelector('h1');
-const h1Alert = e => {
-  alert('addEventListener: Great! You found the heading!');
+tabsContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+
+  // active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// menu fading
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(element => {
+      if (element !== link) element.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
 };
 
-h1.addEventListener('mouseenter', h1Alert);
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+// sticky nav w/ intersectionObserver API
 
-setTimeout(() => h1.removeEventListener('mouseenter', h1Alert), 3000);
+const header = document.querySelector('.header');
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const sitckyNav = entries => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(sitckyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+// reveal section
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// img lazy loading
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '+200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
